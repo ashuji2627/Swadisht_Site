c607f177f105eb301f7b8931b74a7ed9ebd4e4a9
Ashutosh Sahu <133403501+ashuji2627@users.noreply.github.com>
2024-12-30 23:51:14 +0530

    Add files via upload

---
+++ Frontend/src/constants/restaurantConstant.js
@@ -0,0 +1,16 @@
+export const All_RESTAURANTS_REQUEST="All_RESTAURANTS_REQUEST";
+//make sure all letter are in capital letters
+export const All_RESTAURANTS_SUCCESS="All_RESTAURANTS_SUCCESS";
+//above one is verbal commmand given to the Reducer
+
+export const All_RESTAURANTS_FAIL ="All_RESTAURANTS_FAIL";
+//give some commands,to verbal  or dispatch for the filtering part of the restaurant
+export const CLEAR_ERROR="CLEAR_ERROR";
+//FOR clearing any error , we are also use some command
+
+export const SORT_BY_RATINGS ="SORT_BY_RATINGS";
+//FOR sorting filtering use command
+export const SORT_BY_REVIEWS ="SORT_BY_REVIEWS";
+//for sorting on the basis of reviews
+export const TOGGLE_VEG_ONLY ="TOGGLE_VEG_ONLY";
+//for only veg part in the restaurant
